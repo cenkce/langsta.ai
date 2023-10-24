62d1841dee35ea1177eaf6f711bfc9e01e521568
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2023-10-25 00:38:58 +0300

    feat: apikey form

--- src/app-sidepanel/SidepanelApp.tsx
+++ src/app-sidepanel/SidepanelApp.tsx
@@ -2,20 +2,12 @@ import { useLocalstorageSync } from "../api/storage/useLocalstorageSync";
 import {
   ContentContextAtom,
   ContentStorage,
-  TranslationTextTask,
-  deleteTranslation,
-  useUserContentState,
 } from "../domain/content/ContentContext.atom";
-import { ContentMessageDispatch } from "../domain/content/messages";
 import { ArtBoard } from "../ui/ArtBoard";
-import { FlexRow } from "../ui/FlexRow";
-import { LoadingIcon } from "../ui/icons/LoadingIcon";
-import { TrashIcon } from "../ui/icons/TrashIcon";
 import styles from "./SidepanelApp.module.scss";
+import { Translations } from "./Translations";
 
 export const SidepanelApp = () => {
-  const [userContent] = useUserContentState();
-  const tasks = Object.values(userContent?.translation || {});
   
   useLocalstorageSync({
     debugKey: "content-sidepanel",
@@ -30,61 +22,9 @@ export const SidepanelApp = () => {
       theme="cupcake"
     >
       <div className={styles.container}>
-        {tasks
-          .sort((a, b) => b.createdAt - a.createdAt)
-          .map((task) => {
-            return (
-              <TranslationRow
-                key={task.taskId}
-                onDelete={(id) => {
-                  deleteTranslation(id);
-                  ContentMessageDispatch({
-                    type: "backend/delete-task",
-                    payload: {
-                      task: id,
-                    },
-                  });
-                }}
-                task={task}
-              />
-            );
-          })}
+        <Translations />
       </div>
     </ArtBoard>
   );
 };
 
-const TranslationRow = ({
-  task,
-  onDelete,
-}: {
-  onDelete: (taskId: string) => void;
-  task: TranslationTextTask;
-}) => {
-  return (
-    <div className={`${styles.translationItem} collapse collapse-arrow bg-base-200 `} style={{ fontSize: "12px" }}>
-      <input type="radio" name={"sidebar-translations"} />
-      <div className={styles.title}>
-        <pre style={{ whiteSpace: "break-spaces" }}>
-          {task?.selectedText.trim().replaceAll("\n", "\n\n")}
-        </pre>
-      </div>
-      <div className="collapse-content">
-        {task.status === "progress" ? (
-          <FlexRow className={styles.content}>
-            <LoadingIcon />
-            translating ...
-          </FlexRow>
-        ) : (
-          <pre className={`${styles.content} text-neutral-600`}>
-            {task.result?.trim().replaceAll("\n", "\n\n")}
-          </pre>
-        )}
-      </div>
-      <FlexRow className="place-content-between">
-        <TrashIcon onClick={() => onDelete(task.taskId || "")}></TrashIcon>
-        <div>{new Date(task.createdAt).toLocaleString()}</div>
-      </FlexRow>
-    </div>
-  );
-};
---
+++ src/app-sidepanel/Translations.tsx
@@ -0,0 +1,67 @@
+import {
+  TranslationTextTask,
+  deleteTranslation,
+  useUserContentState
+} from "../domain/content/ContentContext.atom";
+import { ContentMessageDispatch } from "../domain/content/messages";
+import { FlexRow } from "../ui/FlexRow";
+import { LoadingIcon } from "../ui/icons/LoadingIcon";
+import { TrashIcon } from "../ui/icons/TrashIcon";
+import styles from "./SidepanelApp.module.scss";
+
+export const Translations = () => {
+  const [userContent] = useUserContentState();
+  const tasks = Object.values(userContent?.translation || {});
+
+  return tasks
+    .sort((a, b) => b.createdAt - a.createdAt)
+    .map((task) => {
+      return (
+        <TranslationRow
+          key={task.taskId}
+          onDelete={(id) => {
+            deleteTranslation(id);
+            ContentMessageDispatch({
+              type: "backend/delete-task",
+              payload: {
+                task: id,
+              },
+            });
+          }}
+          task={task} />
+      );
+    });
+};
+const TranslationRow = ({
+  task, onDelete,
+}: {
+  onDelete: (taskId: string) => void;
+  task: TranslationTextTask;
+}) => {
+  return (
+    <div className={`${styles.translationItem} collapse collapse-arrow bg-base-200 `} style={{ fontSize: "12px" }}>
+      <input type="radio" name={"sidebar-translations"} />
+      <div className={styles.title}>
+        <pre style={{ whiteSpace: "break-spaces" }}>
+          {task?.selectedText.trim().replaceAll("\n", "\n\n")}
+        </pre>
+      </div>
+      <div className="collapse-content">
+        {task.status === "progress" ? (
+          <FlexRow className={styles.content}>
+            <LoadingIcon />
+            translating ...
+          </FlexRow>
+        ) : (
+          <pre className={`${styles.content} text-neutral-600`}>
+            {task.result?.trim().replaceAll("\n", "\n\n")}
+          </pre>
+        )}
+      </div>
+      <FlexRow className="place-content-between">
+        <TrashIcon onClick={() => onDelete(task.taskId || "")}></TrashIcon>
+        <div>{new Date(task.createdAt).toLocaleString()}</div>
+      </FlexRow>
+    </div>
+  );
+};
