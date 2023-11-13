83d1172c2e1ef6dcfbd268de68a47646b2eaaa33
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2023-11-14 00:09:10 +0200

    feat: translation-card positioning improvement

--- src/app-content/ContentMarkerBadge.tsx
+++ src/app-content/ContentMarkerBadge.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { TranslationContentCard } from "./TranslationContentCard";
 import { ImageIcon } from "../ui/icons/ImageIcon";
 import { useSubscribeTranslationTask } from "../domain/content/ContentContext.atom";
@@ -11,7 +11,7 @@ export type ContentMarkerBadgeType = {
   loading: boolean;
   clicked: boolean;
   text: string;
-  taskId?: string
+  taskId?: string;
 };
 type ContentMarkerBadgeProps = ContentMarkerBadgeType & {
   onClose?: (marker: ContentMarkerBadgeType) => void;
@@ -23,38 +23,62 @@ export const ContentMarkerBadge = (props: ContentMarkerBadgeProps) => {
   const markerRef = useRef<HTMLDivElement | null>(null);
   const { onClose, ...badgeData } = props;
   const task = useSubscribeTranslationTask(props.taskId);
-  const loading = task?.status === 'progress';
+  const loading = task?.status === "progress";
+  useEffect(() => {
+    if (markerRef.current) {
+      const handler = () => {
+        if (markerRef.current) {
+          const bodyRect = document.body.getBoundingClientRect();
+          const ownRect = markerRef.current?.getBoundingClientRect();
+          const ww = ownRect?.width || 0;
+          markerRef.current.style.left =
+            Math.max(Math.min(props.left - ww / 2, bodyRect.right - ww), 0) +
+            "px";
+          markerRef.current.style.top =
+            Math.max(props.top - (ownRect?.height || 0), window.scrollY) + "px";
+        }
+      };
+      const observer = new MutationObserver(handler);
+      handler();
+
+      observer.observe(markerRef.current, {
+        attributes: false,
+        childList: true,
+        subtree: false,
+      });
+
+      return () => observer.disconnect();
+    }
+  }, []);
 
   return (
-    <>
-      <div
-        className={"ContentMarkerBadge "}
-        style={{
-          zIndex: Number.MAX_SAFE_INTEGER,
-          left: props.left,
-          top: props.top,
-        }}
-        onMouseDown={(e) => e.stopPropagation()}
-        ref={markerRef}
-      >
-        {props.clicked ? (
-          <TranslationContentCard
-            onTranslate={() => props.onTranslate?.(badgeData)}
-            onClose={() => onClose?.(badgeData)}
-            loading={loading}
-          >
-            <pre style={{whiteSpace: 'break-spaces'}}>{task?.result || props.text}</pre>
-          </TranslationContentCard>
-        ) : (
-          <ImageIcon
-            onClick={(e) => {
-              e.stopPropagation();
-              props.onClick?.(badgeData);
-            }}
-            iconUrl="logo.png"
-          />
-        )}
-      </div>
-    </>
+    <div
+      className={"ContentMarkerBadge "}
+      style={{
+        zIndex: Number.MAX_SAFE_INTEGER,
+      }}
+      onMouseDown={(e) => e.stopPropagation()}
+      ref={markerRef}
+    >
+      {props.clicked ? (
+        <TranslationContentCard
+          onTranslate={() => props.onTranslate?.(badgeData)}
+          onClose={() => onClose?.(badgeData)}
+          loading={loading}
+        >
+          <pre style={{ whiteSpace: "break-spaces" }}>
+            {task?.result || props.text}
+          </pre>
+        </TranslationContentCard>
+      ) : (
+        <ImageIcon
+          onClick={(e) => {
+            e.stopPropagation();
+            props.onClick?.(badgeData);
+          }}
+          iconUrl="logo.png"
+        />
+      )}
+    </div>
   );
 };
