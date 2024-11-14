20596937242e9f58564550dea1343de127107720
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2024-11-15 01:58:52 +0200

    feat: crosswords new features
    - Autofocus next cell after inputted a letter
    - Autofocus prev cell when clicked backspace
    - Autofocus prev cell when clicked left arrow
    - Autofocus next cell when clicked right arrow

--- @apps/app/src/app-sidepanel-study-mode/crosswords/Crosswords.tsx
+++ @apps/app/src/app-sidepanel-study-mode/crosswords/Crosswords.tsx
@@ -3,7 +3,7 @@ import { CrosswordLayout, generateLayout } from "crossword-layout-generator";
 import styles from "./Crosswords.module.css";
 import { Center, Flex, List, Title } from "@mantine/core";
 import { classNames } from "@espoojs/utils";
-import { upperFirst } from "@mantine/hooks";
+import { upperFirst, useSetState } from "@mantine/hooks";
 
 export interface WordEntry {
   id?: number;
@@ -41,28 +41,27 @@ const CrosswordGrid: React.FC<{
   onCellChange?: (row: number, col: number, value: string) => void;
 }> = ({ layout }) => {
   const [userInputs, setUserInputs] = useState<Map<string, string>>(new Map());
-  const [highlightedClue, setHighlightedClue] = useState<string[] | undefined>(
-    [],
-  );
+  const [highlightedClue, setHighlightedClue] = useSetState<{
+    focused: string[] | undefined;
+    hovered: string[] | undefined;
+  }>({ focused: [], hovered: [] });
 
   const getClueByPosition = (x: number, y: number) => {
-    return layout.result
-      .filter((res) => {
-        if (res.orientation === "across") {
-          return (
-            res.startx <= x &&
-            res.starty === y &&
-            res.startx + res.answer.length > x
-          );
-        } else {
-          return (
-            res.startx === x &&
-            res.starty <= y &&
-            res.starty + res.answer.length > y
-          );
-        }
-      })
-      .map(({ clue }) => clue);
+    return layout.result.filter((res) => {
+      if (res.orientation === "across") {
+        return (
+          res.startx <= x &&
+          res.starty === y &&
+          res.startx + res.answer.length > x
+        );
+      } else {
+        return (
+          res.startx === x &&
+          res.starty <= y &&
+          res.starty + res.answer.length > y
+        );
+      }
+    });
   };
 
   const matchUserInputsWithAnswerByPosition = () => {
@@ -93,13 +92,92 @@ const CrosswordGrid: React.FC<{
     return matchedAnswers;
   };
 
+  const lastSelectedWordRef = React.useRef<string | null>(null);
+
+  const findAndFocusNextCell = ({
+    rowIndex,
+    colIndex,
+    direction,
+    recursive,
+    backspace,
+  }: {
+    rowIndex: number;
+    colIndex: number;
+    direction?: number;
+    backspace?: boolean;
+    recursive?: boolean;
+  }) => {
+    const focusedCell = document.getElementById(
+      `${colIndex}-${rowIndex}`,
+    ) as HTMLInputElement;
+    if (
+      focusedCell &&
+      (focusedCell.value ||
+        (!focusedCell.value && (backspace || recursive)) ||
+        direction !== undefined)
+    ) {
+      const factor =
+        direction !== undefined
+          ? direction
+          : backspace && !focusedCell.value
+            ? -1
+            : 1;
+
+      const result = getClueByPosition(colIndex + 1, rowIndex + 1).find(
+        (item) =>
+          lastSelectedWordRef.current
+            ? lastSelectedWordRef.current === item?.answer
+            : true,
+      );
+
+      lastSelectedWordRef.current = result?.answer || null;
+
+      if (result) {
+        const { startx, starty, orientation } = result;
+        let nextCol: number | null = null;
+        let nextRow: number | null = null;
+        if (
+          orientation === "across" &&
+          colIndex < startx + result.answer.length - 1
+        ) {
+          nextCol = colIndex + factor;
+          nextRow = rowIndex;
+        } else if (
+          orientation === "down" &&
+          rowIndex < starty + result.answer.length - 1
+        ) {
+          nextCol = colIndex;
+          nextRow = rowIndex + factor;
+        }
+
+        if (focusedCell && nextCol !== null && nextRow !== null) {
+          const nextCellElement = document.getElementById(
+            `${nextCol}-${nextRow}`,
+          );
+          if (nextCellElement?.tagName !== "INPUT") {
+            // find next proper cell
+            findAndFocusNextCell({
+              rowIndex: nextRow,
+              colIndex: nextCol,
+              backspace,
+              direction: direction,
+              recursive: true,
+            });
+          } else if (nextCellElement) {
+            nextCellElement?.focus();
+          }
+        }
+      }
+    }
+  };
+
   return (
     <Flex
       className={styles["crossword-grid"]}
       direction={"row"}
       columnGap={"md"}
     >
-      <Flex gap={1} direction={'column'}>
+      <Flex gap={1} direction={"column"}>
         {layout.table.map((row, rowIndex) => (
           <div className={styles["crossword-row"]} key={rowIndex}>
             {row.map((cell, colIndex) => (
@@ -117,11 +195,15 @@ const CrosswordGrid: React.FC<{
                   ) ? (
                     <Center
                       onMouseOver={() =>
-                        setHighlightedClue(
-                          getClueByPosition(colIndex + 1, rowIndex + 1),
-                        )
+                        setHighlightedClue({
+                          hovered: getClueByPosition(
+                            colIndex + 1,
+                            rowIndex + 1,
+                          ).map(({ clue }) => clue),
+                        })
                       }
-                      onMouseOut={() => setHighlightedClue([])}
+                      id={`${colIndex}-${rowIndex}`}
+                      onMouseOut={() => setHighlightedClue({ hovered: [] })}
                     >
                       {userInputs
                         .get(`${rowIndex}-${colIndex}`)
@@ -131,26 +213,70 @@ const CrosswordGrid: React.FC<{
                     <input
                       type="text"
                       maxLength={1}
+                      data-cell={`cell-${colIndex}-${rowIndex}`}
+                      id={`${colIndex}-${rowIndex}`}
                       value={
                         userInputs
                           .get(`${rowIndex}-${colIndex}`)
                           ?.toUpperCase() || ""
                       }
-                      onMouseOver={() =>
-                        setHighlightedClue(
-                          getClueByPosition(colIndex + 1, rowIndex + 1),
-                        )
+                      onKeyDown={(e) => {
+                        if (e.key === "Backspace") {
+                          if (!e.currentTarget.value)
+                            // focus on previous cell
+                            findAndFocusNextCell({
+                              rowIndex,
+                              colIndex,
+                              backspace: true,
+                            });
+                          else {
+                            setUserInputs((answers) => {
+                              const newAnswers = new Map(answers);
+                              newAnswers.set(`${rowIndex}-${colIndex}`, "");
+                              return newAnswers;
+                            });
+                          }
+                        } else if (e.key === "ArrowRight") {
+                          findAndFocusNextCell({
+                            rowIndex,
+                            colIndex,
+                            direction: 1,
+                          });
+                        } else if (e.key === "ArrowLeft") {
+                          findAndFocusNextCell({
+                            rowIndex,
+                            colIndex,
+                            direction: -1,
+                          });
+                        } else if (/^[a-zA-Z]$/.test(e.key)) {
+                          e.currentTarget.value = e.key.toUpperCase();
+                          findAndFocusNextCell({ rowIndex, colIndex });
+                          setUserInputs((answers) => {
+                            const newAnswers = new Map(answers);
+                            newAnswers.set(`${rowIndex}-${colIndex}`, e.key);
+                            return newAnswers;
+                          });
+                        }
+                      }}
+                      onFocus={() =>
+                        setHighlightedClue({
+                          focused: getClueByPosition(
+                            colIndex + 1,
+                            rowIndex + 1,
+                          ).map(({ clue }) => clue),
+                        })
                       }
-                      onMouseOut={() => setHighlightedClue([])}
-                      onChange={(e) => {
-                        setUserInputs((ansers) => {
-                          const newAnswers = new Map(ansers);
-                          newAnswers.set(
-                            `${rowIndex}-${colIndex}`,
-                            e.target.value,
-                          );
-                          return newAnswers;
+                      onBlur={() => setHighlightedClue({ focused: [] })}
+                      onMouseOver={() => {
+                        setHighlightedClue({
+                          hovered: getClueByPosition(
+                            colIndex + 1,
+                            rowIndex + 1,
+                          ).map(({ clue }) => clue),
                         });
+                      }}
+                      onMouseOut={() => {
+                        setHighlightedClue({ hovered: [] });
                       }}
                     />
                   ))}
@@ -160,17 +286,19 @@ const CrosswordGrid: React.FC<{
         ))}
       </Flex>
       <Flex direction={"column"}>
-        <List type="ordered">
+        <List type="ordered" className={styles["clues-list"]}>
           {layout.result.map(({ clue }) => (
             <List.Item
-              onMouseOver={() => setHighlightedClue([clue])}
-              onMouseOut={() => setHighlightedClue([])}
+              onMouseOver={() => setHighlightedClue({ hovered: [clue] })}
+              onMouseOut={() => setHighlightedClue({ hovered: [] })}
               style={{ cursor: "pointer" }}
               key={clue}
               data-clue={clue}
               className={classNames(
-                highlightedClue?.includes(clue),
+                highlightedClue?.hovered?.includes(clue),
                 styles.highlighted,
+                highlightedClue?.focused?.includes(clue),
+                styles.focused,
               )}
             >
               {upperFirst(clue)}
