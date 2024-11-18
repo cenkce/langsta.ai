a0a8e2099307fe7f82d084d40ad190f61badbaca
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2024-11-18 02:24:16 +0200

    fix: add undefined and null check

--- @packages/utilities/src/classNames.ts
+++ @packages/utilities/src/classNames.ts
@@ -8,8 +8,8 @@
 export function classNames(...args: (string | undefined | null | boolean)[]) {
   return args.reduce<string>((acc, curr, i) => {
     return (typeof args[i] === "string" && (args[i] as string).trim() !== ""
-      ? i > 0 && typeof args[i - 1] === "boolean"
-        ? args[i - 1] === true
+      ? i > 0 && (typeof args[i - 1] !== "string")
+        ? !!args[i - 1] === true
           ? `${acc} ${curr}`
           : acc
         : `${acc} ${curr}`
